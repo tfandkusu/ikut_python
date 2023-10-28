c5c88e6a4cb75ca69fa49d5d9ea39d542eb5e224
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-10-29 00:07:57 +0900

    入力層、出力層生成を作成

--- s2_data.py
+++ s2_data.py
@@ -1,15 +1,19 @@
 import csv
 import os
 from label_path import LabelPath
+import cv2
+import random
+import tensorflow as tf
 
 LABELS: list[str] = ["start", "end", "kill", "death", "other"]
+CSV_PATH = "data/s24_auto_ml.csv"
 IMAGE_DIR = "data/03_s2train/"
 
 
 class S2Data:
     def __init__(self):
         all_images: list[LabelPath] = []
-        with open("data/s24_auto_ml.csv") as f:
+        with open(CSV_PATH) as f:
             for row in csv.reader(f):
                 uri = row[0]
                 label = row[1]
@@ -17,8 +21,17 @@ class S2Data:
                     filename = uri.split("/")[-1]
                     path = os.path.join(IMAGE_DIR, filename)
                     all_images.append(LabelPath(label=label, path=path))
-        for image in all_images:
-            print(image)
         # all_images を訓練データとテストデータに分ける
         self.train = all_images[:-5000]
         self.test = all_images[-5000:]
+
+    def make_xy(self):
+        "訓練データの入力x, 出力yのペアを作成する"
+        # 訓練データの入力x 作成
+        index = random.randint(0, len(self.train) - 1)
+        img = cv2.imread(self.train[index].path)
+        x = cv2.resize(img, (224, 224))
+        # 訓練データの入力y 作成
+        label = self.train[index].label
+        y = tf.keras.utils.to_categorical(LABELS.index(label), len(LABELS))
+        return x, y
--- s2_train.py
+++ s2_train.py
@@ -1,4 +1,4 @@
 from s2_data import S2Data
 
-
 data = S2Data()
+print(data.make_xy())
